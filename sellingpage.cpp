09a440d62e1fc369ee7ea1784a73239a8034db34
CQJTUWilliam <1584075475@qq.com>
2019-12-24 18:54:32 +0800

    优化销售页面的布局

--- sellingpage.cpp
+++ sellingpage.cpp
@@ -37,4 +37,39 @@ void SellingPage::setViews()
     ui->CartTableView->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
 
     ui->CartTableView->setModel(model);
+
+    //加载供应商数据
+    QSqlQuery query;
+    QString querySupplierDataStr = "select supplier_name from supplier;";
+    query.exec(querySupplierDataStr);
+
+    int row = 0;
+    while (query.next()) {
+        ui->SupplierComboBox->addItem(query.value(0).toString());
+        row++;
+    }
+}
+
+//添加到购物车
+void SellingPage::on_AddToCartButton_clicked()
+{
+
+}
+
+//下订单
+void SellingPage::on_PlaceOrderButton_clicked()
+{
+
+}
+
+//供应商改变,加载商品数据
+void SellingPage::on_SupplierComboBox_currentIndexChanged(int index)
+{
+
+}
+
+//商品改变，更新商品详细数据
+void SellingPage::on_CommodityComboBox_currentIndexChanged(int index)
+{
+
 }
--- sellingpage.h
+++ sellingpage.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QStandardItemModel>
+#include <QSqlQuery>
 
 namespace Ui {
 class SellingPage;
@@ -15,6 +16,15 @@ class SellingPage : public QWidget
 public:
     explicit SellingPage(QWidget *parent = nullptr);
     ~SellingPage();
+
+private slots:
+    void on_AddToCartButton_clicked();
+
+    void on_SupplierComboBox_currentIndexChanged(int index);
+
+    void on_CommodityComboBox_currentIndexChanged(int index);
+
+    void on_PlaceOrderButton_clicked();
 
 private:
     Ui::SellingPage *ui;
